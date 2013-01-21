e4560f51e6a60214d93ffe2282923b794941e7ac
Lee Teague <lee@perfectsensedigital.com>
2013-01-21 11:15:59 -0600

    Upgrades oracle schema to match latest mysql schema

--- etc/oracle/3-grants.sql
+++ etc/oracle/3-grants.sql
@@ -1,8 +1,8 @@
 
 GRANT ALL ON dari.Record TO dari;
-GRANT ALL ON dari.RecordLocation2 TO dari;
-GRANT ALL ON dari.RecordString3 TO dari;
-GRANT ALL ON dari.RecordNumber2 TO dari;
-GRANT ALL ON dari.RecordUuid2 TO dari;
+GRANT ALL ON dari.RecordLocation3 TO dari;
+GRANT ALL ON dari.RecordString4 TO dari;
+GRANT ALL ON dari.RecordNumber3 TO dari;
+GRANT ALL ON dari.RecordUuid3 TO dari;
 GRANT ALL ON dari.RecordUpdate TO dari;
 GRANT ALL ON dari.Symbol TO dari;
