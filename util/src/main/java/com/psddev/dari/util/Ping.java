9f6996e5308f7e1649aec0508b1653b1c3b789b1
Hyoo Lim <hyoolim@gmail.com>
2013-03-08 08:38:14 -0500

    Changes Ping.Static.pingAll to use a cached list of ping classes for performance.

--- util/src/main/java/com/psddev/dari/util/Ping.java
+++ util/src/main/java/com/psddev/dari/util/Ping.java
@@ -2,6 +2,7 @@ package com.psddev.dari.util;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /** Pings a service to ensure that it's operational. */
 public interface Ping {
@@ -14,6 +15,13 @@ public interface Ping {
 
     /** {@link Ping} utility methods. */
     public static final class Static {
+
+        private static final PullThroughValue<Set<Class<? extends Ping>>> PING_CLASSES = new PullThroughValue<Set<Class<? extends Ping>>>() {
+            @Override
+            protected Set<Class<? extends Ping>> produce() {
+                return ObjectUtils.findClasses(Ping.class);
+            }
+        };
 
         /**
          * Pings a service using an instance of the given {@code pingClass}.
@@ -36,7 +44,7 @@ public interface Ping {
          */
         public static Map<Class<?>, Throwable> pingAll() {
             Map<Class<?>, Throwable> errors = new HashMap<Class<?>, Throwable>();
-            for (Class<? extends Ping> pingClass : ObjectUtils.findClasses(Ping.class)) {
+            for (Class<? extends Ping> pingClass : PING_CLASSES.get()) {
                 errors.put(pingClass, ping(pingClass));
             }
             return errors;
