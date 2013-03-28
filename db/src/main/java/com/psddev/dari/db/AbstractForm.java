09a9bbeca1804cf260f63136f624935961ee53ab
Dan Slaughter <dan@perfectsensedigital.com>
2013-03-28 17:01:38 -0400

    Adds AbstractForm for subclassing that simplifies the use of FormTag and InputTag together, and simulates a typical form display and submission workflow.

---
+++ db/src/main/java/com/psddev/dari/db/AbstractForm.java
@@ -0,0 +1,59 @@
+package com.psddev.dari.db;
+
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.psddev.dari.util.FormFilter;
+import com.psddev.dari.util.FormProcessor2;
+
+public abstract class AbstractForm extends FormWriter2 implements FormProcessor2 {
+
+    public AbstractForm() {
+        this(null);
+    }
+
+    public AbstractForm(Writer writer) {
+        super(writer);
+    }
+
+    @Override
+    public final Object process(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        State state = getState(request);
+
+        if (FormFilter.Static.isFormSubmitted(this, request)) {
+            try {
+                InputTag.Static.updateObject(this, state, request);
+
+                processState(state, request, response);
+                return state;
+
+            } catch (IOException e) {
+                throw e;
+
+            } catch (Throwable e) {
+                e.printStackTrace(); // TODO: Remove debug statement
+            }
+        }
+
+        Writer originalDelegate = getDelegate();
+        try {
+            setDelegate(response.getWriter());
+            writeState(state);
+
+        } finally {
+            setDelegate(originalDelegate);
+        }
+
+        return state;
+    }
+
+    protected abstract State getState(HttpServletRequest request);
+
+    protected abstract void processState(State state, HttpServletRequest request, HttpServletResponse response) throws IOException;
+
+    protected abstract void writeState(State state) throws IOException;
+}
