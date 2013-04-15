624373ed0883f22f02dfdbb60cbaf335555953c8
Jeremy Collins <jeremy@perfectsensedigital.com>
2013-04-15 19:58:09 -0400

    Add install target to Makefile for the Dari UDF.

--- mysql/udf/lib_mysqludf_dari.sql
+++ mysql/udf/lib_mysqludf_dari.sql
@@ -1,3 +1,8 @@
+DROP FUNCTION IF EXISTS dari_get_fields;
 CREATE FUNCTION dari_get_fields RETURNS STRING SONAME 'lib_mysqludf_dari.so';
+
+DROP FUNCTION IF EXISTS dari_get_value;
 CREATE FUNCTION dari_get_value RETURNS STRING SONAME 'lib_mysqludf_dari.so';
+
+DROP FUNCTION IF EXISTS dari_increment_metric;
 CREATE FUNCTION dari_increment_metric RETURNS STRING SONAME 'lib_mysqludf_dari.so';
