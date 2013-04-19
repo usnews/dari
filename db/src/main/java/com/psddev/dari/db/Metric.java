e4140358c1a1db5a1a56d73ebbf2ff63742b6242
Hyoo Lim <hyoolim@gmail.com>
2013-04-19 07:04:47 -0700

    Cleans up Metric API.

--- db/src/main/java/com/psddev/dari/db/Metric.java
+++ db/src/main/java/com/psddev/dari/db/Metric.java
@@ -6,50 +6,43 @@ import java.util.Map;
 
 import org.joda.time.DateTime;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-
 @Metric.Embedded
 public class Metric extends Record {
 
-    private transient final ObjectField metricField;
     private transient final MetricDatabase metricDatabase;
 
-    //private static final Logger LOGGER = LoggerFactory.getLogger(Metric.class);
+    protected Metric(State state, String fieldInternalName) {
+        ObjectField field = state.getField(fieldInternalName);
 
-    public Metric(State state, String metricFieldInternalName) {
-        this.metricField = state.getField(metricFieldInternalName);
-        this.metricDatabase = new MetricDatabase(state, metricField.getUniqueName());
-        this.metricDatabase.setEventDateProcessor(metricField.as(MetricDatabase.FieldData.class).getEventDateProcessor());
+        this.metricDatabase = new MetricDatabase(state, field.getUniqueName());
+        this.metricDatabase.setEventDateProcessor(field.as(MetricDatabase.FieldData.class).getEventDateProcessor());
     }
 
-    // Implicit null dimension
-    public void increment(double amount) {
-        incrementDimensionOnEventDate(null, amount, null);
+    /**
+     * Increases the metric value by the given {@code amount} right now.
+     */
+    public void incrementNow(double amount) {
+        increment(amount, null, null);
     }
 
-    // Implicit null dimension
-    public void incrementOnEventDate(double amount, DateTime eventDate) {
-        incrementDimensionOnEventDate(null, amount, eventDate);
-    }
-
-    // Explicit dimension
-    public void incrementDimension(String dimensionValue, double amount) {
-        incrementDimensionOnEventDate(dimensionValue, amount, null);
-    }
-
-    // Explicit dimension
-    public void incrementDimensionOnEventDate(String dimensionValue, double amount, DateTime eventDate) {
+    /**
+     * Increases the metric value by the given {@code amount} and associate it
+     * with the given {@code dimension} and {@code date}.
+     *
+     * @param dimension May be {@code null}.
+     * @param date If {@code null}, right now.
+     */
+    public void increment(double amount, String dimension, DateTime date) {
         try {
-            metricDatabase.setEventDate(eventDate);
-            metricDatabase.incrementMetric(dimensionValue, amount);
+            metricDatabase.setEventDate(date);
+            metricDatabase.incrementMetric(dimension, amount);
         } catch (SQLException e) {
             throw new DatabaseException(metricDatabase.getDatabase(), "Error in MetricDatabase.incrementMetric() : " + e.getLocalizedMessage());
         }
     }
 
-    // All dimensions
-    public void delete() {
+    /** Deletes all metric values. */
+    public void deleteAll() {
         try {
             metricDatabase.deleteMetric();
         } catch (SQLException e) {
@@ -57,100 +50,59 @@ public class Metric extends Record {
         }
     }
 
-    //////////////////////////////////////////////////
-
-    // Implicit null dimension
-    public double getValue() {
-        return getDimensionValueBetween(null, null, null);
-    }
-
-    // Implicit null dimension
-    public double getValueBetween(DateTime startTimestamp, DateTime endTimestamp) {
-        return getDimensionValueBetween(null, startTimestamp, endTimestamp);
-    }
-
-    // Explicit dimension
-    public double getDimensionValue(String dimensionValue) {
-        return getDimensionValueBetween(dimensionValue, null, null);
-    }
-
-    // Explicit dimension
-    public double getDimensionValueBetween(String dimensionValue, DateTime startTimestamp, DateTime endTimestamp) {
+    /**
+     * Returns the metric value associated with the given {@code dimension}
+     * between the given {@code start} and {@code end}.
+     *
+     * @param dimension May be {@code null}.
+     * @param start If {@code null}, beginning of time.
+     * @param end If {@code null}, end of time.
+     */
+    public double getByDimensionBetween(String dimension, DateTime start, DateTime end) {
         try {
-            metricDatabase.setQueryDateRange(startTimestamp, endTimestamp);
-            Double metricValue = metricDatabase.getMetric(dimensionValue);
+            metricDatabase.setQueryDateRange(start, end);
+            Double metricValue = metricDatabase.getMetric(dimension);
             return metricValue == null ? 0.0 : metricValue;
         } catch (SQLException e) {
             throw new DatabaseException(metricDatabase.getDatabase(), "Error in MetricDatabase.getMetric() : " + e.getLocalizedMessage());
         }
     }
 
-    // Implicit null dimension
-    public Map<DateTime, Double> getTimeline() {
-        return getDimensionTimelineBetween(null, null, null, null);
+    /**
+     * Returns the metric value associated with the given {@code dimension}.
+     *
+     * @param dimension May be {@code null}.
+     */
+    public double getByDimension(String dimension) {
+        return getByDimensionBetween(dimension, null, null);
     }
 
-    // Implicit null dimension
-    public Map<DateTime, Double> getTimeline(MetricInterval metricInterval) {
-        return getDimensionTimelineBetween(null, metricInterval, null, null);
+    /**
+     * Returns the metric value associated with the main ({@code null})
+     * dimension between the given {@code start} and {@code end}.
+     */
+    public double get(DateTime start, DateTime end) {
+        return getByDimensionBetween(null, start, end);
     }
 
-    // Implicit null dimension
-    public Map<DateTime, Double> getTimelineBetween(MetricInterval metricInterval, DateTime startTimestamp, DateTime endTimestamp) {
-        return getDimensionTimelineBetween(null, metricInterval, startTimestamp, startTimestamp);
+    /**
+     * Returns the metric value associated with the main ({@code null})
+     * dimension.
+     */
+    public double get() {
+        return getByDimensionBetween(null, null, null);
     }
 
-    // Explicit dimension
-    public Map<DateTime, Double> getDimensionTimeline(String dimensionValue) {
-        return getDimensionTimelineBetween(dimensionValue, null, null, null);
-    }
-
-    // Explicit dimension
-    public Map<DateTime, Double> getDimensionTimeline(String dimensionValue, MetricInterval metricInterval) {
-        return getDimensionTimelineBetween(dimensionValue, metricInterval, null, null);
-    }
-
-    // Explicit dimension
-    public Map<DateTime, Double> getDimensionTimelineBetween(String dimensionValue, MetricInterval metricInterval, DateTime startTimestamp, DateTime endTimestamp) {
+    /**
+     * Returns the sum of all metric values in each dimension between the given
+     * {@code start} and {@code end}.
+     *
+     * @param start If {@code null}, beginning of time.
+     * @param end If {@code null}, end of time.
+     */
+    public double getSumBetween(DateTime start, DateTime end) {
         try {
-            metricDatabase.setQueryDateRange(startTimestamp, endTimestamp);
-            Map<DateTime, Double> metricTimeline = metricDatabase.getMetricTimeline(dimensionValue, metricInterval);
-            return metricTimeline == null ? new HashMap<DateTime, Double>() : metricTimeline;
-        } catch (SQLException e) {
-            throw new DatabaseException(metricDatabase.getDatabase(), "Error in MetricDatabase.getMetricTimeline() : " + e.getLocalizedMessage());
-        }
-    }
-
-    // Sum of all dimensions
-    public Map<DateTime, Double> getSumTimeline() {
-        return getSumTimelineBetween(null, null, null);
-    }
-
-    // Sum of all dimensions
-    public Map<DateTime, Double> getSumTimeline(MetricInterval metricInterval) {
-        return getSumTimelineBetween(metricInterval, null, null);
-    }
-
-    // Sum of all dimensions
-    public Map<DateTime, Double> getSumTimelineBetween(MetricInterval metricInterval, DateTime startTimestamp, DateTime endTimestamp) {
-        try {
-            metricDatabase.setQueryDateRange(startTimestamp, endTimestamp);
-            Map<DateTime, Double> metricTimeline = metricDatabase.getMetricSumTimeline(metricInterval);
-            return metricTimeline == null ? new HashMap<DateTime, Double>() : metricTimeline;
-        } catch (SQLException e) {
-            throw new DatabaseException(metricDatabase.getDatabase(), "Error in MetricDatabase.getSumTimeline() : " + e.getLocalizedMessage());
-        }
-    }
-
-    // Sum of all dimensions
-    public double getSum() {
-        return getSumBetween(null, null);
-    }
-
-    // Sum of all dimensions
-    public double getSumBetween(DateTime startTimestamp, DateTime endTimestamp) {
-        try {
-            metricDatabase.setQueryDateRange(startTimestamp, endTimestamp);
+            metricDatabase.setQueryDateRange(start, end);
             Double metricValue = metricDatabase.getMetricSum();
             return metricValue == null ? 0.0 : metricValue;
         } catch (SQLException e) {
@@ -158,15 +110,24 @@ public class Metric extends Record {
         }
     }
 
-    // All dimensions
-    public Map<String, Double> getValues() {
-        return getValuesBetween(null, null);
+    /**
+     * Returns the sum of all metric values in each dimension.
+     */
+    public double getSum() {
+        return getSumBetween(null, null);
     }
 
-    // All dimensions
-    public Map<String, Double> getValuesBetween(DateTime startTimestamp, DateTime endTimestamp) {
+    /**
+     * Groups the metric values between the given {@code start} and {@code end}
+     * by each dimension.
+     *
+     * @param start If {@code null}, beginning of time.
+     * @param end If {@code null}, end of time.
+     * @return Never {@code null}.
+     */
+    public Map<String, Double> groupByDimensionBetween(DateTime start, DateTime end) {
         try {
-            metricDatabase.setQueryDateRange(startTimestamp, endTimestamp);
+            metricDatabase.setQueryDateRange(start, end);
             Map<String, Double> metricValues = metricDatabase.getMetricValues();
             return metricValues == null ? new HashMap<String, Double>() : metricValues;
         } catch (SQLException e) {
@@ -174,6 +135,50 @@ public class Metric extends Record {
         }
     }
 
-    //////////////////////////////////////////////////
+    /**
+     * Groups the metric values by each dimension.
+     *
+     * @return Never {@code null}.
+     */
+    public Map<String, Double> groupByDimension() {
+        return groupByDimensionBetween(null, null);
+    }
 
+    /**
+     * Groups the metric values associated with the given {@code dimension}
+     * between the given {@code start} and {@code end} by the given
+     * {@code interval}.
+     *
+     * @param dimension May be {@code null}.
+     * @param start If {@code null}, beginning of time.
+     * @param end If {@code null}, end of time.
+     * @return Never {@code null}.
+     */
+    public Map<DateTime, Double> groupByDate(String dimension, MetricInterval interval, DateTime start, DateTime end) {
+        try {
+            metricDatabase.setQueryDateRange(start, end);
+            Map<DateTime, Double> metricTimeline = metricDatabase.getMetricTimeline(dimension, interval);
+            return metricTimeline == null ? new HashMap<DateTime, Double>() : metricTimeline;
+        } catch (SQLException e) {
+            throw new DatabaseException(metricDatabase.getDatabase(), "Error in MetricDatabase.getMetricTimeline() : " + e.getLocalizedMessage());
+        }
+    }
+
+    /**
+     * Groups the sum of all metric values between the given {@code start} and
+     * {@code end} by the given {@code interval}.
+     *
+     * @param start If {@code null}, beginning of time.
+     * @param end If {@code null}, end of time.
+     * @return Never {@code null}.
+     */
+    public Map<DateTime, Double> groupSumByDate(MetricInterval interval, DateTime start, DateTime end) {
+        try {
+            metricDatabase.setQueryDateRange(start, end);
+            Map<DateTime, Double> metricTimeline = metricDatabase.getMetricSumTimeline(interval);
+            return metricTimeline == null ? new HashMap<DateTime, Double>() : metricTimeline;
+        } catch (SQLException e) {
+            throw new DatabaseException(metricDatabase.getDatabase(), "Error in MetricDatabase.getSumTimeline() : " + e.getLocalizedMessage());
+        }
+    }
 }
