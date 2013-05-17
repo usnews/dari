a9f42b66a53d677386d5f8f5cf8f13b61a398415
Hyoo Lim <hyoolim@gmail.com>
2013-05-17 15:58:12 -0400

    Adds SmsProvider and TwilioSmsProvider for sending SMS messages.

---
+++ util/src/main/java/com/psddev/dari/util/SmsProvider.java
@@ -0,0 +1,73 @@
+package com.psddev.dari.util;
+
+/** For sending SMS messages. */
+public interface SmsProvider extends SettingsBackedObject {
+
+    /** Setting key for default SMS provider name. */
+    public static final String DEFAULT_SMS_PROVIDER_SETTING = "dari/defaultSmsProvider";
+
+    /** Setting key prefix for all SMS provider configuration. */
+    public static final String SETTING_PREFIX = "dari/smsProvider";
+
+    /**
+     * Sends the given SMS {@code message} from the given {@code fromNumber}
+     * to the given {@code toNumber}.
+     *
+     * @param fromNumber Can't be blank.
+     * @param toNumber Can't be blank.
+     * @param message Can't be blank.
+     */
+    public void send(String fromNumber, String toNumber, String message);
+
+    /**
+     * {@link SmsProvider} utility methods.
+     *
+     * <p>The factory method, {@link #getInstance}, uses {@link Settings}
+     * to construct instances.</p>
+     */
+    public static final class Static {
+
+        protected static final PullThroughCache<String, SmsProvider>
+                INSTANCES = new PullThroughCache<String, SmsProvider>() {
+
+            @Override
+            public SmsProvider produce(String name) {
+                return Settings.newInstance(SmsProvider.class, SETTING_PREFIX + "/" + name);
+            }
+        };
+
+        private Static() {
+        }
+
+        /**
+         * Returns the SMS provider associated with the given {@code name}.
+         *
+         * @param name If blank, returns the default SMS provider.
+         * @return Never {@code null}, but throws an exception if the SMS
+         * provider associated with the given {@code name} can't be found or
+         * isn't configured..
+         */
+        public static SmsProvider getInstance(String name) {
+            if (ObjectUtils.isBlank(name)) {
+                name = Settings.get(String.class, DEFAULT_SMS_PROVIDER_SETTING);
+
+                if (ObjectUtils.isBlank(name)) {
+                    throw new IllegalStateException(
+                            "No default SMS provider setting found!");
+                }
+            }
+
+            return INSTANCES.get(name);
+        }
+
+        /**
+         * Returns the default SMS provider.
+         *
+         * @return Never {@code null}, but throws an exception if the default
+         * SMS provider can't be found or isn't configured.
+         */
+        public static SmsProvider getDefault() {
+            return getInstance(null);
+        }
+    }
+}
---
+++ util/src/main/java/com/psddev/dari/util/TwilioSmsProvider.java
@@ -0,0 +1,104 @@
+package com.psddev.dari.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Map;
+
+import javax.xml.bind.DatatypeConverter;
+
+/**
+ * {@link SmsProvider} backed by <a href="http://www.twilio.com/">Twilio</a>.
+ */
+public class TwilioSmsProvider implements SmsProvider {
+
+    public static final String ACCOUNT_SID_SUB_SETTING = "accountSid";
+    public static final String AUTH_TOKEN_SUB_SETTING = "authToken";
+    public static final String DEFAULT_FROM_NUMBER_SUB_SETTING = "defaultFromNumber";
+
+    private String accountSid;
+    private String authToken;
+    private String defaultFromNumber;
+
+    public String getAccountSid() {
+        return accountSid;
+    }
+
+    public void setAccountSid(String accountSid) {
+        this.accountSid = accountSid;
+    }
+
+    public String getAuthToken() {
+        return authToken;
+    }
+
+    public void setAuthToken(String authToken) {
+        this.authToken = authToken;
+    }
+
+    public String getDefaultFromNumber() {
+        return defaultFromNumber;
+    }
+
+    public void setDefaultFromNumber(String defaultFromNumber) {
+        this.defaultFromNumber = defaultFromNumber;
+    }
+
+    @Override
+    public void initialize(String settingsKey, Map<String, Object> settings) {
+        setAccountSid(ObjectUtils.to(String.class, settings.get(ACCOUNT_SID_SUB_SETTING)));
+        setAuthToken(ObjectUtils.to(String.class, settings.get(AUTH_TOKEN_SUB_SETTING)));
+        setDefaultFromNumber(ObjectUtils.to(String.class, settings.get(DEFAULT_FROM_NUMBER_SUB_SETTING)));
+    }
+
+    @Override
+    public void send(String fromNumber, String toNumber, String message) {
+        if (ObjectUtils.isBlank(fromNumber)) {
+            fromNumber = getDefaultFromNumber();
+        }
+
+        try {
+            HttpURLConnection conn = (HttpURLConnection) new URL(
+                    "https://api.twilio.com/2010-04-01/Accounts/" +
+                    getAccountSid() +
+                    "/SMS/Messages.json").openConnection();;
+
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("Authorization", "Basic " + DatatypeConverter.printBase64Binary((getAccountSid() + ":" + getAuthToken()).getBytes(StringUtils.UTF_8)));
+            conn.setDoOutput(true);
+
+            OutputStream output = conn.getOutputStream();
+
+            try {
+                writeParameter(output, "From", fromNumber);
+                output.write('&');
+                writeParameter(output, "To", toNumber);
+                output.write('&');
+                writeParameter(output, "Body", message);
+
+            } finally {
+                output.close();
+            }
+
+            InputStream input = conn.getInputStream();
+
+            try {
+                IoUtils.toString(input, StringUtils.UTF_8);
+
+            } finally {
+                input.close();
+            }
+
+        } catch (IOException error) {
+            error.printStackTrace();
+        }
+    }
+
+    private void writeParameter(OutputStream output, String name, String value) throws IOException {
+        output.write(StringUtils.encodeUri(name).getBytes(StringUtils.UTF_8));
+        output.write('=');
+        output.write(StringUtils.encodeUri(value).getBytes(StringUtils.UTF_8));
+    }
+}
