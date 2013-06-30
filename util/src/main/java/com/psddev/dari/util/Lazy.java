4cc53f5f78bb6d47173e7e9b3739bcd1000ea976
Hyoo Lim <hyoolim@gmail.com>
2013-06-30 13:34:35 -0400

    Adds Lazy#destroy hook so that the subclasses can run additional logic when the value is reset and improves documentation.

--- util/src/main/java/com/psddev/dari/util/Lazy.java
+++ util/src/main/java/com/psddev/dari/util/Lazy.java
@@ -23,21 +23,28 @@ package com.psddev.dari.util;
 public abstract class Lazy<T> {
 
     private T value;
+    private volatile boolean created;
 
     private final Once createOnce = new Once() {
+
         @Override
         protected void run() throws Exception {
             value = create();
+            created = true;
         }
     };
 
     /**
      * Creates and returns the value.
+     *
+     * @return May be {@code null}.
      */
     protected abstract T create() throws Exception;
 
     /**
      * Returns the value, creating it using {@link #create} if necessary.
+     *
+     * @return May be {@code null} if {@link #create} returns {@code null}.
      */
     public final T get() {
         createOnce.ensure();
@@ -45,10 +52,28 @@ public abstract class Lazy<T> {
     }
 
     /**
+     * Runs any additional logic to destroy the value if necessary.
+     * The default implementation of this method does nothing.
+     *
+     * @param value May be {@code null} if {@link #create} returned
+     * {@code null}.
+     */
+    protected void destroy(T value) {
+    }
+
+    /**
      * Resets so that the next invocation of {@link #get} can call
      * {@link #create} again.
      */
     public final void reset() {
-        createOnce.reset();
+        try {
+            if (created) {
+                destroy(value);
+            }
+
+        } finally {
+            created = false;
+            createOnce.reset();
+        }
     }
 }
