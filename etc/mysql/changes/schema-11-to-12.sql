8d99f6581e9b5e926a59e66063694d4868d78e7e
Lee Teague <lee@perfectsensedigital.com>
2013-11-15 08:17:57 -0600

    Adds commented out DROP/RENAME statements for migration to schema-12.

--- etc/mysql/changes/schema-11-to-12.sql
+++ etc/mysql/changes/schema-11-to-12.sql
@@ -14,14 +14,48 @@ INSERT IGNORE INTO RecordUuid3 (id, typeId, symbolId, value)
 SELECT RecordUuid2.id, Record.typeId, RecordUuid2.symbolId, RecordUuid2.value FROM RecordUuid2 JOIN Record ON RecordUuid2.id = Record.id;
 
 /*
+
+-- DROP OLD TABLES:
+
 DROP TABLE RecordString3;
-DROP VIEW RecordString3_d;
 DROP TABLE RecordLocation2;
-DROP VIEW RecordLocation2_d;
 DROP TABLE RecordRegion;
-DROP VIEW RecordRegion_d;
 DROP TABLE RecordNumber2;
-DROP VIEW RecordNumber2_d;
 DROP TABLE RecordUuid2;
+
+DROP VIEW RecordString3_d;
+DROP VIEW RecordLocation2_d;
+DROP VIEW RecordRegion_d;
+DROP VIEW RecordNumber2_d;
 DROP VIEW RecordUuid2_d;
+
+-- OR --
+
+-- RENAME OLD TABLES:
+ALTER TABLE RecordString3 RENAME TO RecordString3_bak;
+ALTER TABLE RecordLocation2 RENAME TO RecordLocation2_bak;
+ALTER TABLE RecordRegion RENAME TO RecordRegion_bak;
+ALTER TABLE RecordNumber2 RENAME TO RecordNumber2_bak;
+ALTER TABLE RecordUuid2 RENAME TO RecordUuid2_bak;
+
+-- REVERT BACK TO OLD TABLES:
+ALTER TABLE RecordString3_bak RENAME TO RecordString3;
+ALTER TABLE RecordLocation2_bak RENAME TO RecordLocation2;
+ALTER TABLE RecordRegion_bak RENAME TO RecordRegion;
+ALTER TABLE RecordNumber2_bak RENAME TO RecordNumber2;
+ALTER TABLE RecordUuid2_bak RENAME TO RecordUuid2;
+
+-- DROP NEW TABLES:
+DROP TABLE RecordString4;
+DROP TABLE RecordLocation3;
+DROP TABLE RecordRegion2;
+DROP TABLE RecordNumber3;
+DROP TABLE RecordUuid3;
+
+DROP VIEW RecordString4_d;
+DROP VIEW RecordLocation3_d;
+DROP VIEW RecordRegion2_d;
+DROP VIEW RecordNumber3_d;
+DROP VIEW RecordUuid3_d;
+
 */
