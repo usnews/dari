15c698ce53e815fcafa056959cc1022dddd6ca7d
Hyoo Lim <hyoolim@gmail.com>
2013-12-04 09:11:30 -0500

    Adds ugc to list of abbreviations for StringUtils#toLabel.

--- util/src/main/java/com/psddev/dari/util/StringUtils.java
+++ util/src/main/java/com/psddev/dari/util/StringUtils.java
@@ -38,7 +38,7 @@ public final class StringUtils {
 
     private static final Set<String>
             ABBREVIATIONS = new HashSet<String>(Arrays.asList(
-            "cms", "css", "id", "js", "seo", "uri", "url"));
+            "cms", "css", "id", "js", "seo", "ugc", "uri", "url"));
 
     private static final char[] HEX_CHARACTERS = {
             '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
