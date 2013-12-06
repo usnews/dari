usnews/dari

commit ece7fba44c0c8e97a0b55f453d3a41a1001df27e

Adds @BootstrapTypeMappable.
This provides the ability to map remote IDs to local IDs for types such
as Directory and ToolUser in the CMS, similar to how ObjectType IDs are
mapped.

db/src/main/java/com/psddev/dari/db/BootstrapImportTask.java
@@ -6,8 +6,10 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.regex.Matcher;
@@ -41,8 +43,8 @@ class BootstrapImportTask extends Task {
     private final Map<UUID,ObjectType> unknownTypes = new HashMap<UUID,ObjectType>();
     private boolean needsTranslation;
     private final Pattern uuidPattern = Pattern.compile("[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}", Pattern.CASE_INSENSITIVE);
-    private final Map<UUID,UUID> remoteToLocalTypeIdMap = new HashMap<UUID,UUID>();
-    private final Map<String,String> remoteToLocalTypeIdStringMap = new HashMap<String,String>();
+    private final Map<UUID,UUID> remoteToLocalIdMap = new HashMap<UUID,UUID>();
+    private final Map<String,String> remoteToLocalIdStringMap = new HashMap<String,String>();
     private final TypeReference<Map<String,Object>> MAP_STRING_OBJECT_TYPE = new TypeReference<Map<String,Object>>() {};
 
     public BootstrapImportTask(Database database, String filename, InputStream fileInputStream, boolean deleteFirst, int numWriters, int commitSize) {
@@ -105,7 +107,11 @@ class BootstrapImportTask extends Task {
             }
             UUID localObjTypeId = database.getEnvironment().getTypeByClass(ObjectType.class).getId();
             UUID globalsId = new UUID(-1L, -1L);
+            Set<String> typeNames = new HashSet<String>();
+            boolean isAllTypes = false;
+            Map<String,String> typeMapTypeFields = new HashMap<String,String>();
             if (headers.get(BootstrapPackage.Static.TYPES_HEADER).trim().equals(BootstrapPackage.Static.ALL_TYPES_HEADER_VALUE)) {
+                isAllTypes = true;
                 if (deleteFirst) {
                     LOGGER.info("Deleting all records in database to load " + filename);
                     for (Object obj : Query.fromAll().where("_type != ?", localObjTypeId).and("_id != ?", globalsId).noCache().using(database).resolveToReferenceOnly().iterable(100)) {
@@ -120,6 +126,7 @@ class BootstrapImportTask extends Task {
                 for (String remoteTypeName : headers.get(BootstrapPackage.Static.TYPES_HEADER).split(",")) {
                     remoteTypeName = remoteTypeName.trim();
                     ObjectType localType = database.getEnvironment().getTypeByName(remoteTypeName);
+                    typeNames.add(remoteTypeName);
                     if (localType == null) {
                         localType = new ObjectType();
                         localType.setInternalName(remoteTypeName);
@@ -138,6 +145,16 @@ class BootstrapImportTask extends Task {
                         }
                     }
                 }
+
+                if (headers.containsKey(BootstrapPackage.Static.TYPE_MAP_HEADER) && headers.get(BootstrapPackage.Static.TYPE_MAP_HEADER) != null && !"".equals(headers.get(BootstrapPackage.Static.TYPE_MAP_HEADER).trim())) {
+                    for (String typeMap : headers.get(BootstrapPackage.Static.TYPE_MAP_HEADER).split(",")) {
+                        String[] parts = typeMap.split("/");
+                        if (parts.length == 2) {
+                            typeMapTypeFields.put(parts[0], parts[1]);
+                        }
+                    }
+                }
+
             }
 
             // block until deleters are done
@@ -167,15 +184,14 @@ class BootstrapImportTask extends Task {
             }
             LOGGER.info("Importing data from " + filename + " . . . ");
             int numRows = 0;
-            ObjectType objType = database.getEnvironment().getTypeByClass(ObjectType.class);
-            boolean afterObjectTypes = false;
+            boolean afterMappingTypes = false;
             while (null != (line = reader.readLine())) {
                 if (!shouldContinue()) break;
                 line = line.trim();
                 if ("".equals(line)) continue;
                 if (line.startsWith("#")) continue;
                 if (! line.startsWith("{") || ! line.endsWith("}")) throw new RuntimeException("Invalid line in input file: " + line);
-                line = translateTypeIds(line);
+                line = translateIds(line);
                 Map<String, Object> stateMap = ObjectUtils.to(MAP_STRING_OBJECT_TYPE, ObjectUtils.fromJson(line));
                 UUID globalId = new UUID(-1L, -1L);
                 UUID zeroTypeId = new UUID(0L, 0L);
@@ -206,22 +222,33 @@ class BootstrapImportTask extends Task {
                         LOGGER.error("Unknown type in line: " + line);
                         continue;
                     }
-                    if (!afterObjectTypes && objType.equals(type)) {
-                        ObjectType localType = database.getEnvironment().getTypeByName(ObjectUtils.to(String.class, stateMap.get("internalName")));
-                        if (localType != null) {
-                            remoteToLocalTypeIdMap.put(ObjectUtils.to(UUID.class, stateMap.get("_id")), localType.getId());
-                            stateMap.put("_id", localType.getId());
+
+                    if (!afterMappingTypes && typeMapTypeFields.containsKey(ObjectUtils.to(String.class, stateMap.get("_type")))) {
+                        String typeMapField = typeMapTypeFields.get(ObjectUtils.to(String.class, stateMap.get("_type")));
+                        Object localObj = Query.fromType(type).where(typeMapField + " = ?", ObjectUtils.to(String.class, stateMap.get(typeMapField))).first();
+                        if (localObj != null) {
+                            remoteToLocalIdMap.put(ObjectUtils.to(UUID.class, stateMap.get("_id")), ((Recordable) localObj).getState().getId());
                         }
-                    } else if (!afterObjectTypes) {
-                        afterObjectTypes = true;
+                        if (localObj == null || isAllTypes || typeNames.contains(type.getInternalName())) {
+                            record.getState().setResolveToReferenceOnly(true);
+                            record.getState().setValues(stateMap);
+                            saveQueue.add(record);
+                        }
+                    } else if (!afterMappingTypes) {
+                        afterMappingTypes = true;
                         // this is the first line after all of the objectTypes, potentially need to re-parse it.
-                        prepareTypeIdTranslation();
-                        line = translateTypeIds(line);
+                        prepareIdTranslation();
+                        line = translateIds(line);
                         stateMap = ObjectUtils.to(MAP_STRING_OBJECT_TYPE, ObjectUtils.fromJson(line));
+                        record.getState().setResolveToReferenceOnly(true);
+                        record.getState().setValues(stateMap);
+                        saveQueue.add(record);
+                    } else {
+                        record.getState().setResolveToReferenceOnly(true);
+                        record.getState().setValues(stateMap);
+                        saveQueue.add(record);
                     }
-                    record.getState().setResolveToReferenceOnly(true);
-                    record.getState().setValues(stateMap);
-                    saveQueue.add(record);
+
                     setProgressIndex(++numRows);
                 } catch (RuntimeException t) {
                     LOGGER.error("Error when saving state at "+stateMap.get("_id")+": ", t);
@@ -264,34 +291,34 @@ class BootstrapImportTask extends Task {
         }
     }
 
-    private void prepareTypeIdTranslation() {
-        needsTranslation = ! remoteToLocalTypeIdMap.isEmpty();
-        for (Map.Entry<UUID,UUID> entry : remoteToLocalTypeIdMap.entrySet()) {
-            remoteToLocalTypeIdStringMap.put(entry.getKey().toString(), entry.getValue().toString());
+    private void prepareIdTranslation() {
+        needsTranslation = ! remoteToLocalIdMap.isEmpty();
+        for (Map.Entry<UUID,UUID> entry : remoteToLocalIdMap.entrySet()) {
+            remoteToLocalIdStringMap.put(entry.getKey().toString(), entry.getValue().toString());
         }
     }
 
-    private String translateTypeIds(String line) {
+    private String translateIds(String line) {
         if (!needsTranslation) return line;
         StringBuilder newLine = new StringBuilder();
-        Matcher typeIdMatcher = uuidPattern.matcher(line);
+        Matcher idMatcher = uuidPattern.matcher(line);
         int cursor = 0;
         boolean found = false;
-        while (typeIdMatcher.find()) {
-            int start = typeIdMatcher.start();
-            int end = typeIdMatcher.end();
+        while (idMatcher.find()) {
+            int start = idMatcher.start();
+            int end = idMatcher.end();
             newLine.append(line.substring(cursor, start));
-            String remoteTypeId = line.substring(start, end);
-            String localTypeId;
-            if ((localTypeId = remoteToLocalTypeIdStringMap.get(remoteTypeId)) != null) {
+            String remoteId = line.substring(start, end);
+            String localId;
+            if ((localId = remoteToLocalIdStringMap.get(remoteId)) != null) {
                 found = true;
-                newLine.append(localTypeId);
+                newLine.append(localId);
             } else {
-                newLine.append(remoteTypeId);
+                newLine.append(remoteId);
             }
             cursor = end;
             ObjectType unknownType;
-            if ((unknownType = unknownTypes.remove(localTypeId)) != null) {
+            if ((unknownType = unknownTypes.remove(localId)) != null) {
                 saveQueue.add(unknownType);
             }
         }

db/src/main/java/com/psddev/dari/db/BootstrapPackage.java
@@ -83,6 +83,10 @@ public class BootstrapPackage extends Record {
 
         private Set<String> packages;
 
+        private Set<String> typeMappableGroups;
+
+        private String typeMappableUniqueKey;
+
         public Set<String> getPackageNames() {
             if (packages == null) packages = new HashSet<String>();
             return packages;
@@ -90,6 +94,25 @@ public class BootstrapPackage extends Record {
 
         public void setPackageNames(Set<String> packages) {
             this.packages = packages;
+        }
+
+        public Set<String> getTypeMappableGroups() {
+            if (typeMappableGroups == null) {
+                typeMappableGroups = new HashSet<String>();
+            }
+            return typeMappableGroups;
+        }
+
+        public void setTypeMappableGroups(Set<String> typeMappableGroups) {
+            this.typeMappableGroups = typeMappableGroups;
+        }
+
+        public String getTypeMappableUniqueKey() {
+            return typeMappableUniqueKey;
+        }
+
+        public void setTypeMappableUniqueKey(String typeMappableUniqueKey) {
+            this.typeMappableUniqueKey = typeMappableUniqueKey;
         }
 
     }
@@ -101,6 +124,7 @@ public class BootstrapPackage extends Record {
         public static final String PROJECT_HEADER = "Project";
         public static final String DATE_HEADER = "Date";
         public static final String TYPES_HEADER = "Types";
+        public static final String TYPE_MAP_HEADER = "Mapping Types";
         public static final String ROW_COUNT_HEADER = "Row Count";
         public static final String ALL_TYPES_HEADER_VALUE = "ALL";
 
@@ -213,68 +237,151 @@ public class BootstrapPackage extends Record {
         }
 
         public static void writeContents(Database database, BootstrapPackage pkg, Set<ObjectType> additionalTypes, Writer writer, String projectName) throws IOException {
+            boolean first = true;
+            boolean needsObjectTypeMap = false;
+            Set<ObjectType> typeMaps = new HashSet<ObjectType>();
+            ObjectType objType = database.getEnvironment().getTypeByClass(ObjectType.class);
+            Set<ObjectType> exportTypes = new HashSet<ObjectType>();
+            Query<?> query = Query.fromAll().using(database);
+            Set<ObjectType> allTypeMappableTypes = new HashSet<ObjectType>();
+
+            for (ObjectType type : database.getEnvironment().getTypes()) {
+                if (!type.as(TypeData.class).getTypeMappableGroups().isEmpty() &&
+                        type.as(TypeData.class).getTypeMappableUniqueKey() != null) {
+                    allTypeMappableTypes.add(type);
+                }
+            }
+
+            query.where("_type != ?", objType);
+
+            // Package:
             writer.write(PACKAGE_NAME_HEADER + ": ");
             writer.write(pkg.getName());
-            writer.write("\n");
+            writer.write('\n');
+            // Project:
             writer.write(PROJECT_HEADER + ": ");
             writer.write(projectName);
-            writer.write("\n");
+            writer.write('\n');
+            // Date:
             writer.write(DATE_HEADER + ": ");
             writer.write(new DateTime().toString("yyyy-MM-dd HH:mm:ss z"));
-            writer.write("\n");
-            boolean first;
-            ObjectType objType = database.getEnvironment().getTypeByClass(ObjectType.class);
-            Query<?> query = Query.fromAll().using(database);
-            query.where("_type != ?", objType);
+            writer.write('\n');
+
+            // Types:
             writer.write(TYPES_HEADER + ": ");
-            Set<ObjectType> exportTypes = new HashSet<ObjectType>();
             if (pkg.isInit()) {
                 writer.write(ALL_TYPES_HEADER_VALUE);
+                writer.write('\n');
             } else {
-                first = true;
                 exportTypes.addAll(getAllTypes(database, pkg));
                 exportTypes.addAll(getAllTypes(database, additionalTypes));
-                query.where("_type = ?", exportTypes);
-                if (exportTypes.contains(objType)) {
-                    writer.write(objType.getInternalName());
-                    first = false;
+                for (ObjectType typeMappableType : allTypeMappableTypes) {
+                    GETTYPEMAPPABLETYPE: for (ObjectType type : exportTypes) {
+                        for (String group : typeMappableType.as(TypeData.class).getTypeMappableGroups()) {
+                            if (type.getGroups().contains(group)) {
+                                typeMaps.add(typeMappableType);
+                                break GETTYPEMAPPABLETYPE;
+                            }
+                        }
+                    }
                 }
+                GETOBJECTTYPE: for (ObjectType type : exportTypes) {
+                    for (ObjectField field : type.getFields()) {
+                        if (field.getTypes().contains(objType)) {
+                            needsObjectTypeMap = true;
+                            break GETOBJECTTYPE;
+                        }
+                    }
+                }
+
+                query.where("_type = ?", exportTypes);
+
                 if (exportTypes.contains(objType)) {
-                    if (!first) writer.write(","); else first = false;
+                    if (!first) writer.write(','); else first = false;
                     writer.write(objType.getInternalName());
                 }
                 for (ObjectType type : exportTypes) {
                     if (type.equals(objType)) continue;
-                    if (!first) writer.write(","); else first = false;
+                    if (!first) writer.write(','); else first = false;
                     writer.write(type.getInternalName());
                 }
+                writer.write('\n');
+
             }
+
+            if (!typeMaps.isEmpty()) {
+                query.where("_type != ?", typeMaps);
+            }
+
+            // Mapping Types:
+            if (pkg.isInit() || needsObjectTypeMap || !typeMaps.isEmpty()) {
+                first = true;
+                writer.write(TYPE_MAP_HEADER + ": ");
+                if (pkg.isInit() || needsObjectTypeMap) {
+                    writer.write(objType.getInternalName());
+                    writer.write("/internalName");
+                    first = false;
+                }
+                for (ObjectType typeMapType : typeMaps) {
+                    if (!first) {
+                        writer.write(',');
+                    } else {
+                        first = false;
+                    }
+                    writer.write(typeMapType.getInternalName());
+                    writer.write('/');
+                    writer.write(typeMapType.as(TypeData.class).getTypeMappableUniqueKey());
+                }
+                writer.write('\n');
+            }
+
+            // Row Count:
             Long count;
             if (pkg.isInit()) {
                 count = Query.fromAll().using(database).noCache().count();
             } else {
-                count = Query.fromAll().using(database).noCache().where("_type = ?", exportTypes).count();
+                Query<?> countQuery = Query.fromAll().using(database).noCache().where("_type = ?", exportTypes);
+                if (needsObjectTypeMap) {
+                    countQuery.or("_type = ?", objType);
+                }
+                if (!typeMaps.isEmpty()) {
+                    countQuery.or("_type = ?", typeMaps);
+                }
+                count = countQuery.count();
             }
-            writer.write("\n");
             writer.write(ROW_COUNT_HEADER + ": ");
             writer.write(ObjectUtils.to(String.class, count));
-            writer.write("\n\n"); // blank line between headers and data
+            writer.write('\n');
+
+            // blank line between headers and data
+            writer.write('\n');
             writer.flush();
 
-            if (exportTypes.isEmpty() || exportTypes.contains(objType)) {
-                for (ObjectType r : Query.from(ObjectType.class).using(database).noCache().resolveToReferenceOnly().iterable(100)) {
-                    writer.write(ObjectUtils.toJson(r.getState().getSimpleValues(true)));
-                    writer.write("\n");
+            // ObjectType records first
+            if (exportTypes.isEmpty() || exportTypes.contains(objType) || needsObjectTypeMap) {
+                for (Object r : Query.fromType(objType).using(database).noCache().resolveToReferenceOnly().iterable(100)) {
+                    writer.write(ObjectUtils.toJson(((Recordable) r).getState().getSimpleValues(true)));
+                    writer.write('\n');
                 }
             }
 
+            // Then other mapping types
+            for (ObjectType typeMapType : typeMaps) {
+                for (Object r : Query.fromType(typeMapType).using(database).noCache().resolveToReferenceOnly().iterable(100)) {
+                    writer.write(ObjectUtils.toJson(((Recordable) r).getState().getSimpleValues(true)));
+                    writer.write('\n');
+                }
+            }
+
+            // Then everything else
             for (Object o : query.noCache().resolveToReferenceOnly().iterable(100)) {
                 if (o instanceof Record) {
                     Record r = (Record) o;
                     writer.write(ObjectUtils.toJson(r.getState().getSimpleValues(true)));
-                    writer.write("\n");
+                    writer.write('\n');
                 }
             }
+
             writer.flush();
         }
 

db/src/main/java/com/psddev/dari/db/Recordable.java
@@ -352,6 +352,14 @@ public interface Recordable {
         Class<?>[] depends() default { };
     }
 
+    @ObjectType.AnnotationProcessorClass(BootstrapTypeMappableProcessor.class)
+    @Retention(RetentionPolicy.RUNTIME)
+    @Target(ElementType.TYPE)
+    public @interface BootstrapTypeMappable {
+        Class<?>[] groups();
+        String uniqueKey();
+    }
+
     // --- Deprecated ---
 
     /** @deprecated Use {@link Denormalized} instead. */
@@ -830,5 +838,17 @@ class BootstrapPackagesProcessor implements ObjectType.AnnotationProcessor<Recor
             }
         }
         type.as(BootstrapPackage.TypeData.class).setPackageNames(packageNames);
+    }
+}
+
+class BootstrapTypeMappableProcessor implements ObjectType.AnnotationProcessor<Recordable.BootstrapTypeMappable> {
+    @Override
+    public void process(ObjectType type, Recordable.BootstrapTypeMappable annotation) {
+        Set<String> typeMappableGroups = new LinkedHashSet<String>();
+        for (Class<?> group : annotation.groups()) {
+            typeMappableGroups.add(group.getName());
+        }
+        type.as(BootstrapPackage.TypeData.class).setTypeMappableGroups(typeMappableGroups);
+        type.as(BootstrapPackage.TypeData.class).setTypeMappableUniqueKey(annotation.uniqueKey());
     }
 }

commit d91665a7309af727e540e24897f43b392915d46f

Fixes bug introduced in ece7fba.
ObjectTypes should always be treated as "type mappable".

db/src/main/java/com/psddev/dari/db/BootstrapImportTask.java
@@ -185,6 +185,7 @@ class BootstrapImportTask extends Task {
             LOGGER.info("Importing data from " + filename + " . . . ");
             int numRows = 0;
             boolean afterMappingTypes = false;
+            ObjectType objType = database.getEnvironment().getTypeByClass(ObjectType.class);
             while (null != (line = reader.readLine())) {
                 if (!shouldContinue()) break;
                 line = line.trim();
@@ -223,8 +224,11 @@ class BootstrapImportTask extends Task {
                         continue;
                     }
 
-                    if (!afterMappingTypes && typeMapTypeFields.containsKey(ObjectUtils.to(String.class, stateMap.get("_type")))) {
+                    if (!afterMappingTypes && (typeMapTypeFields.containsKey(ObjectUtils.to(String.class, stateMap.get("_type"))) || objType.equals(type))) {
                         String typeMapField = typeMapTypeFields.get(ObjectUtils.to(String.class, stateMap.get("_type")));
+                        if (typeMapField == null && objType.equals(type)) {
+                            typeMapField = "internalName";
+                        }
                         Object localObj = Query.fromType(type).where(typeMapField + " = ?", ObjectUtils.to(String.class, stateMap.get(typeMapField))).first();
                         if (localObj != null) {
                             remoteToLocalIdMap.put(ObjectUtils.to(UUID.class, stateMap.get("_id")), ((Recordable) localObj).getState().getId());