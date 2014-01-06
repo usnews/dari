bba821352808d8861e80717dc568390cce4c7ec8
Lee Teague <lee@perfectsensedigital.com>
2014-01-06 13:26:29 -0600

    Cleanup before merge.

--- util/src/main/java/com/psddev/dari/util/BrightcoveStorageItem.java
+++ util/src/main/java/com/psddev/dari/util/BrightcoveStorageItem.java
@@ -12,7 +12,6 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.UUID;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.http.HttpResponse;
