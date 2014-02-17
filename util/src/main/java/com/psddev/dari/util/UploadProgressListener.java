285f3a189d977350aa0a71468a21ed52d8fd5547
ramana.madupuru <rmadupuru@perfectsensedigital.com>
2014-02-17 13:30:21 -0600

    Updated the logic in listener to update the UploadProgress data in the database

--- util/src/main/java/com/psddev/dari/util/UploadProgressListener.java
+++ util/src/main/java/com/psddev/dari/util/UploadProgressListener.java
@@ -1,5 +1,6 @@
 package com.psddev.dari.util;
-import java.util.List;
+import java.lang.reflect.Method;
+import java.util.Date;
 
 import org.apache.commons.fileupload.ProgressListener;
 import org.slf4j.Logger;
@@ -9,84 +10,68 @@ import org.slf4j.LoggerFactory;
 */
 public class UploadProgressListener implements ProgressListener {
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadProgressListener.class);
-    private long num500Ks = 0;
-    private long theBytesRead = 0;
-    private long theContentLength = -1;
-    private int whichItem = 0;
-    private int percentDone = 0;
-    private boolean contentLengthKnown = false;
-    private String fieldName;
-    private List<String> paramNames;
-    public void setParamNames(List<String> paramNames) {
-        this.paramNames=paramNames;
+    /**
+     * Upload Progress information  will be updated only when the bytes uploaded
+     * exceed the threshold  to reduce the number of update calls
+     */
+    private static long uploadBytesThreshold=250000L;
+    private long  prevBytesRead = 0;
+
+
+    private String uploadProgressKey;
+  
+    public String getUploadProgressKey() {
+        return uploadProgressKey;
     }
+
+    public void setUploadProgressKey(String uploadProgressKey) {
+        this.uploadProgressKey = uploadProgressKey;
+    }
+
     @Override
+    @SuppressWarnings({ "rawtypes", "unchecked" })
     public void update(long bytesRead, long contentLength, int itemIndex) {
-        try {
-        if (contentLength > -1) {
-           contentLengthKnown = true;
+        try {    
+        //Update progres info only if bytes uploaded exceeds uploadBytesThreshold or if the upload is complete
+        if (contentLength > -1 && bytesRead-prevBytesRead  >  uploadBytesThreshold ||bytesRead ==contentLength && bytesRead >  uploadBytesThreshold) {   
+            prevBytesRead=bytesRead;
+            long t1=System.currentTimeMillis();
+            
+            //First, check to see if an 
+            Class uploadProgressClass=Class.forName("com.psddev.dari.db.UploadProgress");
+            Class uploadProgressStatic=Class.forName("com.psddev.dari.db.UploadProgress$Static");
+            Method method=uploadProgressStatic.getDeclaredMethod("find",String.class);
+            Object uploadProgress=method.invoke(uploadProgressStatic,uploadProgressKey);
+            //If upload progress object doesn't exist..create a new one 
+            if (uploadProgress == null) {
+                 uploadProgress=uploadProgressClass.newInstance();
+            }
+            
+            method=uploadProgressClass.getDeclaredMethod("setKey",String.class);
+            method.invoke(uploadProgress, uploadProgressKey);
+            LOGGER.info("Value of upload progress key :" + uploadProgressKey);
+            method=uploadProgressClass.getDeclaredMethod("setBytesRead",Long.class);
+            method.invoke(uploadProgress, bytesRead);
+            method=uploadProgressClass.getDeclaredMethod("setContentLength",Long.class);
+            method.invoke(uploadProgress, contentLength);
+            method=uploadProgressClass.getDeclaredMethod("computePercentDone",(Class[] )null);
+            method.invoke(uploadProgress, (Object[])null);
+            method=uploadProgressClass.getDeclaredMethod("setItemIndex",Integer.class);
+            method.invoke(uploadProgress, itemIndex);  
+            method=uploadProgressClass.getDeclaredMethod("setCreatedAt",Date.class);
+            method.invoke(uploadProgress, new Date());
+            //Save the record
+            method=uploadProgressClass.getMethod("save", (Class[] )null);
+            method.invoke(uploadProgress, (Object[])null);
+            long t2=System.currentTimeMillis();
+            LOGGER.info("Time taken for this method:" + (t2 -t1));
         }
-        theBytesRead = bytesRead;
-        theContentLength = contentLength;
-        whichItem = itemIndex;
-        long nowNum500Ks = bytesRead / 500000;
-        LOGGER.info(bytesRead + " of " + theContentLength + " bytes have been read. Index is:" + itemIndex);
-        // Only run this code once every 500K..we can change it to  1MB
-        if (nowNum500Ks > num500Ks) {
-             num500Ks = nowNum500Ks;
-             if (contentLengthKnown) {
-                 percentDone = (int) Math.round(100.00 * bytesRead / contentLength);
-              }
-        }
-        LOGGER.info("value of percentage done is:" + percentDone);
         } catch (Exception e) {
             LOGGER.error("update method failed" + e);
         }
     }
 
-    
-    public String getMessage() {
-        if (theContentLength == -1) {
-           return new StringBuffer().append(theBytesRead).append(" of Unknown-Total bytes have been read.").toString();
-        } else {
-           return new StringBuffer().append(theBytesRead).append( " of ").append( theContentLength).append(" bytes have been read (").append( percentDone).append("% done).").toString();
-        }
-    }
-    public long getNum500Ks() {
-        return num500Ks;
-    }
-    public void setNum500Ks(long num500Ks) {
-        this.num500Ks = num500Ks;
-    }
-    public long getTheBytesRead() {
-        return theBytesRead;
-    }
-    public void setTheBytesRead(long theBytesRead) {
-        this.theBytesRead = theBytesRead;
-    }
-    public long getTheContentLength() {
-        return theContentLength;
-    }
-    public void setTheContentLength(long theContentLength) {
-        this.theContentLength = theContentLength;
-    }
-    public int getWhichItem() {
-        return whichItem;
-    }
-    public void setWhichItem(int whichItem) {
-        this.whichItem = whichItem;
-    }
-    public int getPercentDone() {
-        return percentDone;
-    }
-    public void setPercentDone(int percentDone) {
-        this.percentDone = percentDone;
-    }
-    public boolean isContentLengthKnown() {
-        return contentLengthKnown;
-    }
-    public void setContentLengthKnown(boolean contentLengthKnown) {
-        this.contentLengthKnown = contentLengthKnown;
-    }
+   
+
 }
 
