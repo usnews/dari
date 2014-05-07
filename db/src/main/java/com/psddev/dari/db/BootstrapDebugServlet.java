2136949b4e50f8664d209b9f76dfb5dbc2241d00
Dan Sisan <dansisan@ds.local>
2014-05-07 12:25:44 -0400

    noticed in MRB, on FFX (but not Chrome) importing .gz files were getting the contentType application/x-tar; for these cases, trust file extension

--- db/src/main/java/com/psddev/dari/db/BootstrapDebugServlet.java
+++ db/src/main/java/com/psddev/dari/db/BootstrapDebugServlet.java
@@ -120,7 +120,7 @@ public class BootstrapDebugServlet extends HttpServlet {
                                 InputStream input = file.getInputStream();
                                 InputStream fileInput = input;
 
-                                if (contentType != null && ("application/x-gzip".equals(contentType) || "application/gzip".equals(contentType))) {
+                                if ((contentType != null && ("application/x-gzip".equals(contentType) || "application/gzip".equals(contentType))) || (fileName != null && fileName.endsWith(".gz"))) {
                                     fileInput = new GZIPInputStream(input);
                                 }
                                 boolean deleteFirst = false;
