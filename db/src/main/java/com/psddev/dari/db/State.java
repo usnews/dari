aa163fba9b6c11f8faeffe58c172a042b5abb0a3
Lee Teague <lee@perfectsensedigital.com>
2014-11-21 07:45:00 -0500

    Bugfix in running ObjectMethods declared by global modifications.

--- db/src/main/java/com/psddev/dari/db/State.java
+++ db/src/main/java/com/psddev/dari/db/State.java
@@ -12,6 +12,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Locale;
@@ -618,6 +619,7 @@ public class State implements Map<String, Object> {
                         value = ((State) value).as(keyClass);
                     }
 
+                    Set<String> checkedMods = new HashSet<String>();
                     for (Class<?> c = keyClass; c != null; c = c.getSuperclass()) {
                         try {
                             Class<?> modC = null;
@@ -626,12 +628,22 @@ public class State implements Map<String, Object> {
                                 keyMethod = objMethod.getJavaMethod(c);
                                 if (keyMethod == null) {
                                     for (String className : State.getInstance(value).getType().getModificationClassNames()) {
+                                        if (checkedMods.contains(className)) {
+                                            continue;
+                                        }
+                                        checkedMods.add(className);
                                         modC = ObjectUtils.getClassByName(className);
                                         if (modC != null) {
                                             keyMethod = objMethod.getJavaMethod(modC);
                                             if (keyMethod != null) {
                                                 break;
                                             }
+                                        }
+                                    }
+                                    if (keyMethod == null && Object.class.equals(c)) {
+                                        modC = ObjectUtils.getClassByName(objMethod.getJavaDeclaringClassName());
+                                        if (modC != null) {
+                                            keyMethod = objMethod.getJavaMethod(modC);
                                         }
                                     }
                                 }
