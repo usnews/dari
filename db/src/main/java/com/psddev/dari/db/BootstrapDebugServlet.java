474415906a8e263269022866832c4ae1d5974766
Ryan-Anderson <ryan@perfectsensedigital.com>
2015-05-05 12:11:59 -0400

    changes fallback project name to dari

--- db/src/main/java/com/psddev/dari/db/BootstrapDebugServlet.java
+++ db/src/main/java/com/psddev/dari/db/BootstrapDebugServlet.java
@@ -68,7 +68,7 @@ public class BootstrapDebugServlet extends HttpServlet {
             String action = wp.param(String.class, "action");
             if (LIVE_DOWNLOAD_BUTTON_TEXT.equals(action) || SNAPSHOT_BUTTON_TEXT.equals(action)) {
                 Properties properties = BuildDebugServlet.getProperties(getServletContext());
-                String projectName = ObjectUtils.firstNonBlank(properties.getProperty("name"), "db-bootstrap");
+                String projectName = ObjectUtils.firstNonBlank(properties.getProperty("name"), "dari");
                 BootstrapPackage pkg = null;
                 if (!StringUtils.isEmpty(pkgName)) {
                     pkg = BootstrapPackage.Static.getPackage(selectedDatabase, pkgName);
