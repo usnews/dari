0731e99c18b1027ac12f622e872216ab95da2b02
Hyoo Lim <hyoolim@gmail.com>
2015-07-20 11:15:05 -0400

    Doc update.

--- util/src/main/java/com/psddev/dari/util/ConversionFunction.java
+++ util/src/main/java/com/psddev/dari/util/ConversionFunction.java
@@ -3,21 +3,34 @@ package com.psddev.dari.util;
 import java.lang.reflect.Type;
 
 /**
- * Function for {@linkplain Converter converting} an arbitrary object
- * into an instance of another type.
+ * For converting an arbitrary object into an instance of another type.
  *
- * @param F Type of the object to be converted.
- * @param T Type of the object to convert into.
+ * @param <F>
+ *        Type of the object to be converted.
+ *
+ * @param <T>
+ *        Type of the object to convert into.
  */
+@FunctionalInterface
 public interface ConversionFunction<F, T> {
 
     /**
      * Converts the given {@code object} into an instance of the given
      * {@code returnType}.
      *
-     * @param converter For when this method needs to convert other values.
-     * @throws NullPointerException If the given {@code returnType} or
-     *         {@code object} is {@code null}.
+     * @param converter
+     *        For when this method needs to convert other values. Can't
+     *        be {@code null}.
+     *
+     * @param returnType
+     *        Can't be {@code null}.
+     *
+     * @param object
+     *        Can't be {@code null}.
+     *
+     * @throws NullPointerException
+     *         If the given {@code returnType} or {@code object} is
+     *         {@code null}.
      */
-    public T convert(Converter converter, Type returnType, F object) throws Exception;
+    T convert(Converter converter, Type returnType, F object) throws Exception;
 }
