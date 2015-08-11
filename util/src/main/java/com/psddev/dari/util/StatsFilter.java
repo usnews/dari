030b463b676c80f89e68a5c0613946799d59a65e
Hyoo Lim <hyoolim@gmail.com>
2015-08-11 11:13:30 -0400

    Stops using custom wrapper to store HTTP response status in StatsFilter since the info is now available natively in Servlet 3.

--- util/src/main/java/com/psddev/dari/util/StatsFilter.java
+++ util/src/main/java/com/psddev/dari/util/StatsFilter.java
@@ -6,18 +6,15 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpServletResponseWrapper;
 
 /**
- * Collects statistics around common servlet operations using
+ * Filter that collects statistics around common servlet operations using
  * {@link Stats}.
  */
 public class StatsFilter extends AbstractFilter {
 
     private static final Stats STATS_INCLUDES = new Stats("JSP Includes");
     private static final Stats STATS_RESPONSES = new Stats("HTTP Responses");
-
-    // --- AbstractFilter support ---
 
     @Override
     protected void doInclude(
@@ -43,47 +40,13 @@ public class StatsFilter extends AbstractFilter {
             FilterChain chain)
             throws IOException, ServletException {
 
-        StatusPreservingResponse statusResponse = new StatusPreservingResponse(response);
         Stats.Timer timer = STATS_RESPONSES.startTimer();
 
         try {
-            chain.doFilter(request, statusResponse);
+            chain.doFilter(request, response);
 
         } finally {
-            timer.stop(String.valueOf(statusResponse.getStatus()));
-        }
-    }
-
-    private static class StatusPreservingResponse extends HttpServletResponseWrapper {
-
-        private int status;
-
-        public StatusPreservingResponse(HttpServletResponse response) {
-            super(response);
-        }
-
-        public int getStatus() {
-            return status;
-        }
-
-        // --- HttpServletResponseWrapper support ---
-
-        @Override
-        public void sendError(int status) throws IOException {
-            this.status = status;
-            super.sendError(status);
-        }
-
-        @Override
-        public void sendError(int status, String message) throws IOException {
-            this.status = status;
-            super.sendError(status, message);
-        }
-
-        @Override
-        public void setStatus(int status) {
-            this.status = status;
-            super.setStatus(status);
+            timer.stop(String.valueOf(response.getStatus()));
         }
     }
 }
