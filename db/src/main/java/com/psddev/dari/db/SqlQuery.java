d405191f8d576ea74137209c1eadd2b46da8c4f9
Lee Teague <lee@perfectsensedigital.com>
2016-03-02 15:59:08 -0500

    Change variable name for clarity

--- db/src/main/java/com/psddev/dari/db/SqlQuery.java
+++ db/src/main/java/com/psddev/dari/db/SqlQuery.java
@@ -57,7 +57,7 @@ class SqlQuery {
     private Join mysqlIndexHint;
     private boolean mysqlIgnoreIndexPrimaryDisabled;
     private boolean forceLeftJoins;
-    private boolean allNotEqualsAll;
+    private boolean hasAnyLimitingPredicates;
 
     private final List<Predicate> recordMetricDatePredicates = new ArrayList<Predicate>();
     private final List<Predicate> recordMetricParentDatePredicates = new ArrayList<Predicate>();
@@ -645,7 +645,9 @@ class SqlQuery {
             boolean hasMissing = false;
             int subClauseCount = 0;
             boolean isNotEqualsAll = PredicateParser.NOT_EQUALS_ALL_OPERATOR.equals(operator);
-            allNotEqualsAll |= isNotEqualsAll;
+            if (!isNotEqualsAll) {
+                hasAnyLimitingPredicates = true;
+            }
 
             if (isNotEqualsAll || PredicateParser.EQUALS_ANY_OPERATOR.equals(operator)) {
                 Query<?> valueQuery = mappedKey.getSubQueryWithComparison(comparisonPredicate);
@@ -1605,7 +1607,7 @@ class SqlQuery {
 
         if (fromClause.length() > 0
                 && !fromClause.contains("LEFT OUTER JOIN")
-                && !allNotEqualsAll
+                && hasAnyLimitingPredicates
                 && !mysqlIgnoreIndexPrimaryDisabled) {
             statementBuilder.append(" /*! IGNORE INDEX (PRIMARY) */");
         }
